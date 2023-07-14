120e24b1cf09a8af2fca412f96ba9aa787a98ebd
Mikhail <leonovm143@gmail.com>
2023-07-14 19:33:01 +0600

    🚀 api done

--- index.js
+++ index.js
@@ -1,56 +1,41 @@
-// const express = require('express');
-// const products = require('../products.json')
-// const app = express();
-
-// // Возвращает все продукты
-// app.get('/products', (req, res) => {
-//     res.json(products);
-// });
-
-// // Возвращает продукт по id
-// app.get('/products/:id', (req, res) => {
-//     const id = parseInt(req.params.id);
-//     const product = products.find(product => product.id === id);
-
-//     if (product) {
-//         res.json(product);
-//     } else {
-//         res.status(404).json({ error: 'Product not found' });
-//     }
-// });
-
-// // Возвращает изображение по пути
-// app.get('/images/:filename', (req, res) => {
-//     const filename = req.params.filename;
-//     res.sendFile(filename, { root: __dirname + '/images' });
-// });
-
-// // Запускаем сервер на порту 5000
-// app.listen(5000, () => {
-//     console.log('Backend приложение запущено на порту 5000');
-// });
-
-// module.exports = app;
-
 const express = require('express')
 const products = require('./products.json')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Express JS on Vercel')
+  res.send('Express JS on Vercel')
 })
 
-app.get('/ping', (req, res) => {
-    res.send(products)
-})
+// Возвращает все продукты
+app.get('/products', (req, res) => {
+  res.json(products);
+});
+
+// Возвращает продукт по id
+app.get('/products/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const product = products.find(product => product.id === id);
+
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).json({ error: 'Product not found' });
+  }
+});
+
+// Возвращает изображение по пути
+app.get('/images/:filename', (req, res) => {
+  const filename = req.params.filename;
+  res.sendFile(filename, { root: __dirname + '/images' });
+});
 
 const port = process.env.PORT || 8080
 
 app.listen(port, (err, res) => {
-    if (err) {
-        console.log(err)
-        return res.status(500).send(err.message)
-    } else {
-        console.log('[INFO] Server Running on port:', port)
-    }
+  if (err) {
+    console.log(err)
+    return res.status(500).send(err.message)
+  } else {
+    console.log('[INFO] Server Running on port:', port)
+  }
 })
